20646dc487f3369b35c82d1b075bd4468e443067
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-04-17 19:14:30 +0100

    dashboard page is created

---
+++ php/dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['user_id'])) {
+    header("location: login.php");
+    exit;
+}
+?>
+
+<h1>welcome <?php echo $_SeSSION['username'];?></h1>
+<a href= "logout.php">Logout</a>
