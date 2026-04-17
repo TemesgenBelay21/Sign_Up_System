e1cc19425414d9c8864d8b4b07c588591a768653
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-04-17 17:58:41 +0100

    login logic

---
+++ php/login.php
@@ -0,0 +1,43 @@
+<?php
+require_once "../config/database.php";
+$errors = [];
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = strtolower(trim($_POST['email']));
+    $password = trim($_POST['password']);
+
+    if(empty($email)) {
+        $errors[] = "email is required";
+    }
+    
+    if(empty($password)) {
+        $errors[] = "password is required";
+    }
+
+    if(!filter_Var($email,FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "invalid email format";
+    }
+    
+    
+    if (!empty($errors)) {
+        foreach ($errors as $error) {
+            echo "<p style='color:red;'>$error</p>";
+        }
+        exit;
+    }
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->execute([$email]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+    IF(!$user) {
+        die("User not found");
+    }
+
+    if(!password_verify($password, $user['password'])){
+        die("incorrect password");
+    }else{
+        echo "welcome back";
+    }
+
+}
+?>
